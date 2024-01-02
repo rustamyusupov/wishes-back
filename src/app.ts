81107fcc97402d22710d72f400253f630cd89a82
Rustam Yusupov <i@rstm.me>
2024-01-02 10:50:00 +0200

    change db path

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
+import url from 'url';
+import path from 'path';
 import jsonServer from 'json-server';
 
+const dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const dbPath = path.join(dirname, 'db.json');
+
 export const app = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(dbPath);
 const middlewares = jsonServer.defaults();
 
 app.use(middlewares);
