eb7784fbc0d3c669e256bd0ceafa3e2a3a8c322d
Rustam Yusupov <i@rstm.me>
2024-01-04 12:37:36 +0200

    remove unnecessary list endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import url from 'url';
 import path from 'path';
 import server from 'json-server';
 
-import db from './db.json';
-
 const dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const dbPath = path.join(dirname, 'db.json');
 
@@ -12,24 +10,6 @@ const router = server.router(dbPath);
 const middlewares = server.defaults();
 
 app.use(middlewares);
-
-app.get('/api/list', (req, res) => {
-  const result = db.categories?.map(category => {
-    const filtered = db.wishes?.filter(wish => wish.categoryId === category.id);
-    const wishes = filtered?.map(wish => {
-      return {
-        ...wish,
-        currency: db.currencies?.find(currency => currency.id === wish.currencyId)?.name,
-        prices: db.prices?.filter(price => price.wishId === wish.id).map(price => price.value),
-      };
-    });
-
-    return { ...category, wishes };
-  });
-
-  res.jsonp(result);
-});
-
 app.use('/api', router);
 
 app.listen(9000, () => {
