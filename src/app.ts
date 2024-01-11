ae6700afe0b545ecdae0d9c96ba93c3ea3fc7341
Rustam Yusupov <i@rstm.me>
2024-01-11 16:50:46 +0200

    init cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-import http from 'http';
+import cors from 'cors';
 import express from 'express';
+import http from 'http';
 
 import { models } from 'middlewares';
 import routes from 'routes';
 
 export const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(models);
