aa800540d70d079f8286c1fc21ad11ef8cfbcb50
Rustam Yusupov <i@rstm.me>
2024-01-11 17:01:35 +0200

    fix port env var

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import { models } from 'middlewares';
 import routes from 'routes';
 
 export const app = express();
+const server = http.createServer(app);
+const port = import.meta.env.VITE_PORT || '9000';
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -13,8 +15,5 @@ app.use(express.json());
 app.use(models());
 
 app.use('/api', routes);
-
-const server = http.createServer(app);
-const port = process.env.PORT || '9000';
 
 server.listen(port, () => console.log(`Server alive on port ${port}`));
