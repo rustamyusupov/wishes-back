7c5a61ecbcbb76dfdc3e73d5ab0f02c8d5876926
Rustam Yusupov <i@rstm.me>
2024-01-13 12:14:22 +0200

    init login and logout controllers

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,21 +1,27 @@
 import type { Request, Response } from 'express';
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 import { User } from 'types';
 
 export const login = async (req: Request, res: Response) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = res.locals.models.users.find((user: User) => user.email === email);
 
-  // if (bcrypt.compareSync(password, user?.password)) {
-  req.session.user = user.id;
-  // }
+  if (bcrypt.compareSync(password, user?.password)) {
+    req.session.user = user.id;
+    res.status(200).send({ user: user.login });
+    return;
+  }
+
+  res.sendStatus(401);
 };
 
-export const logout = async (req: Request) => {
+export const logout = async (req: Request, res: Response) => {
   req.session.destroy(err => {
     if (err) {
       console.error(err);
     }
   });
+
+  res.sendStatus(200);
 };
