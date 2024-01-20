ff548ceee9915708cdb77fe0fd0b4faa5c1b6bb1
Rustam Yusupov <i@rstm.me>
2024-01-20 11:34:13 +0200

    fix auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
-  if (req.session.user) {
+  if (/^-?\d+$/.test(String(req.session.user))) {
     next();
+  } else {
+    res.sendStatus(401);
   }
-
-  res.sendStatus(401);
 };
